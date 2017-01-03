165d512a8aa61a23f968834775b17149b4c4c43b
Steven Bambling <smbambling@gmail.com>
2017-01-03 14:47:21 -0500

    renaming shebangs to wrappers for naming

--- spec/wrappers_spec.rb
+++ spec/wrappers_spec.rb
@@ -7,7 +7,7 @@ describe file('/usr/local/bin/scl-wrapper-ruby193') do
   it { should be_mode '755' }
 end
 
-describe file('/usr/local/bin/scl-shebang-ruby193-ruby') do
+describe file('/usr/local/bin/scl-shebang-ruby193') do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
@@ -21,7 +21,7 @@ describe file('/usr/local/bin/scl-wrapper-python33') do
   it { should be_mode '755' }
 end
 
-describe file('/usr/local/bin/scl-shebang-python33-python') do
+describe file('/usr/local/bin/scl-shebang-python33') do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
