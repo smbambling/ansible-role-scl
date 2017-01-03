7e5beb0c9bfe64f281b5f1e6f90209d1eec4aeab
Steven Bambling <smbambling@gmail.com>
2017-01-03 13:17:02 -0500

    calling shebang/wrapper scripts

--- spec/shebangs_spec.rb
+++ spec/shebangs_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe file('/usr/local/bin/scl-shebang-ruby193') do
+describe file('/usr/local/bin/scl-wrapper-ruby193') do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
@@ -14,7 +14,7 @@ describe file('/usr/local/bin/scl-shebang-ruby193-ruby') do
   it { should be_mode '755' }
 end
 
-describe file('/usr/local/bin/scl-shebang-python33') do
+describe file('/usr/local/bin/scl-wrapper-python33') do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
