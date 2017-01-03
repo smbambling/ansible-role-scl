0f80832e06174f17517743c358fa75832a8f7e0c
Steven Bambling <smbambling@gmail.com>
2017-01-03 12:59:48 -0500

    fixing tests

--- spec/shebangs_spec.rb
+++ spec/shebangs_spec.rb
@@ -7,7 +7,21 @@ describe file('/usr/local/bin/scl-shebang-ruby193') do
   it { should be_mode '755' }
 end
 
+describe file('/usr/local/bin/scl-shebang-ruby193-ruby') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode '755' }
+end
+
 describe file('/usr/local/bin/scl-shebang-python33') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode '755' }
+end
+
+describe file('/usr/local/bin/scl-shebang-python33-python') do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
