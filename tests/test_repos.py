95572739ed16b29f3a5e3ced7c339abb60d8a373
Steven Bambling <smbambling@gmail.com>
2017-07-04 08:58:49 -0400

    fixing tests that require multiple asserts

--- tests/test_repos.py
+++ tests/test_repos.py
@@ -5,17 +5,20 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_file_rpm_gpg_key_centos_sig_sclo(host):
     file = host.file('/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-SCLo')
-    file.exists
-    file.is_file
-    'root' == file.user
-    'root' == file.group
-    '644' == file.mode
+    assert file.exists
+    assert file.is_file
+    assert 'root' == file.user
+    assert 'root' == file.group
+    assert '0644' == oct(file.mode)
 
 
-def test_yumrepo_sclo(host):
+def test_yumrepo_sclo_sclo(host):
     output = host.check_output('yum repolist all')
     assert 'centos-sclo-sclo' in output
     assert 'enabled' in output
 
+
+def test_yumrepo_sclo_rh(host):
+    output = host.check_output('yum repolist all')
     assert 'centos-sclo-rh' in output
-    assert 'enabled' in output
+    assert 'disabled' in output
--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -5,35 +5,35 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_file_scl_wrapper_ruby193(host):
     file = host.file('/usr/local/bin/scl-wrapper-ruby193')
-    file.exists
-    file.is_file
-    'root' == file.user
-    'root' == file.group
-    '755' == file.mode
+    assert file.exists
+    assert file.is_file
+    assert 'root' == file.user
+    assert 'root' == file.group
+    assert '0755' == oct(file.mode)
 
 
 def test_file_scl_shebang_ruby193(host):
     file = host.file('/usr/local/bin/scl-shebang-ruby193')
-    file.exists
-    file.is_file
-    'root' == file.user
-    'root' == file.group
-    '755' == file.mode
+    assert file.exists
+    assert file.is_file
+    assert 'root' == file.user
+    assert 'root' == file.group
+    assert '0755' == oct(file.mode)
 
 
 def test_file_scl_wrapper_python33(host):
     file = host.file('/usr/local/bin/scl-wrapper-python33')
-    file.exists
-    file.is_file
-    'root' == file.user
-    'root' == file.group
-    '755' == file.mode
+    assert file.exists
+    assert file.is_file
+    assert 'root' == file.user
+    assert 'root' == file.group
+    assert '0755' == oct(file.mode)
 
 
 def test_file_scl_shebang_python33(host):
     file = host.file('/usr/local/bin/scl-shebang-python33')
-    file.exists
-    file.is_file
-    'root' == file.user
-    'root' == file.group
-    '755' == file.mode
+    assert file.exists
+    assert file.is_file
+    assert 'root' == file.user
+    assert 'root' == file.group
+    assert '0755' == oct(file.mode)
